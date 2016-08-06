88815a1546ec8e17e9e38dd4b9c6b868407b8b89
gzobl2011 <gzobl2011@gmail.com>
2016-08-06 14:28:40 +0800

    Create cachemarix.R

---
+++ cachemarix.R
@@ -0,0 +1,37 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
+        set <- function(y){
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setInv <- function(inv){
+                i <<- inv
+        }
+        getInv <- function() i
+        list(set = set, get = get, 
+             setInv = setInv, 
+             getInv = getInv)
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        i <- x$getInv()
+        if(!is.null(i)){
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setInv(i)
+        i
+}
